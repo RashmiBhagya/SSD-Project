395e4b40ae0ac628453c20b22d7ff7b1be09676b
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-10-21 10:00:07 +0530

    add changes

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSearch } from "../../context/search";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
 const SearchInput = () => {
   const [values, setValues] = useSearch();
   const navigate = useNavigate();
@@ -9,29 +10,36 @@ const SearchInput = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.get(
-        `/api/v1/product/search/${values.keyword}`
-      );
+      const { data } = await axios.get(`/api/v1/product/search/${values.keyword}`);
       setValues({ ...values, results: data });
       navigate("/search");
     } catch (error) {
       console.log(error);
     }
   };
+
+  const handleInputChange = (e) => {
+    const userInput = e.target.value;
+
+    // Define a regular expression to match valid characters (e.g., letters and numbers)
+    const validCharacters = /^[a-zA-Z0-9\s]+$/;
+
+    if (validCharacters.test(userInput) || userInput === "") {
+      // Valid input or empty input is allowed
+      setValues({ ...values, keyword: userInput });
+    }
+  };
+
   return (
     <div>
-      <form
-        className="d-flex search-form"
-        role="search"
-        onSubmit={handleSubmit}
-      >
+      <form className="d-flex search-form" role="search" onSubmit={handleSubmit}>
         <input
           className="form-control me-2"
           type="search"
           placeholder="Search"
           aria-label="Search"
           value={values.keyword}
-          onChange={(e) => setValues({ ...values, keyword: e.target.value })}
+          onChange={handleInputChange}
         />
         <button className="btn btn-outline-success" type="submit">
           Search
